fbeff5f7f6da30658bcd807d809baa1aba8688e1
ashwakshaik14 <ashwakshaik15@gmail.com>
2025-01-28 21:56:38 +0530

    click

--- routes/dash.js
+++ routes/dash.js
@@ -6,107 +6,6 @@ const UAParser = require("ua-parser-js"); // Install this: npm install ua-parser
 
 
 
-// router.get("/clicks", async (req, res) => {
-//     const { email } = req.query;
-  
-//     if (!email) {
-//       return res.status(400).json({ error: "Email query parameter is required" });
-//     }
-  
-//     try {
-//       // Build the query object
-//       let query = { email };
-  
-//       // Pipeline 1: Total Clicks
-//       const totalClicksPipeline = [
-//         { $match: query },
-//         { $unwind: "$redirectionLogs" },
-//         { $group: { _id: null, totalClicks: { $sum: 1 } } },
-//       ];
-  
-//       // Pipeline 2: Date-wise Clicks
-//       const dateWiseClicksPipeline = [
-//         { $match: query },
-//         { $unwind: "$redirectionLogs" },
-//         {
-//           $group: {
-//             _id: {
-//               $dateToString: { format: "%Y-%m-%d", date: "$redirectionLogs.timestamp" },
-//             },
-//             clicks: { $sum: 1 },
-//           },
-//         },
-//         { $sort: { _id: 1 } }, // Sort by date in ascending order
-//         {
-//           $project: {
-//             date: "$_id",
-//             clicks: 1,
-//             _id: 0,
-//           },
-//         },
-//       ];
-
-  
-//       // Pipeline 3: Device-wise Clicks
-//       const deviceWiseClicksPipeline = [
-//         { $match: query },
-//         { $unwind: "$redirectionLogs" },
-//         {
-//           $group: {
-//             _id: "$redirectionLogs.device",
-//             clicks: { $sum: 1 },
-//           },
-//         },
-//         {
-//           $project: {
-//             device: "$_id",
-//             clicks: 1,
-//             _id: 0,
-//           },
-//         },
-//       ];
-  
-//       // Execute all pipelines
-//       const [totalClicksResult, dateWiseClicksResult, deviceWiseClicksResult] =
-//         await Promise.all([
-//           Url.aggregate(totalClicksPipeline),
-//           Url.aggregate(dateWiseClicksPipeline),
-//           Url.aggregate(deviceWiseClicksPipeline),
-//         ]);
-  
-//       // Extract total clicks
-//       const totalClicks = totalClicksResult[0]?.totalClicks || 0;
-  
-//       // Format date-wise clicks
-//       const dateWiseClicks = dateWiseClicksResult.map((item) => ({
-//         date: item.date,
-//         clicks: item.clicks,
-//       }));
-  
-//       // Format device-wise clicks
-//       const deviceWiseClicks = deviceWiseClicksResult.map((item) => ({
-//         device: item.device,
-//         clicks: item.clicks,
-//       }));
-  
-//       // Send the response
-//       res.status(200).json({
-//         totalClicks,
-//         dateWiseClicks,
-//         deviceWiseClicks,
-//       });
-//     } catch (error) {
-//       console.error("Error fetching clicks data:", error.message || error);
-//       res.status(500).json({ error: "Error fetching clicks data" });
-//     }
-//   });
-  
-
-  
-// module.exports = router;
-
-
-
 router.get("/clicks", async (req, res) => {
     const { email } = req.query;
   
@@ -146,6 +45,7 @@ router.get("/clicks", async (req, res) => {
           },
         },
       ];
+
   
       // Pipeline 3: Device-wise Clicks
       const deviceWiseClicksPipeline = [
@@ -177,15 +77,11 @@ router.get("/clicks", async (req, res) => {
       // Extract total clicks
       const totalClicks = totalClicksResult[0]?.totalClicks || 0;
   
-      // Compute cumulative date-wise clicks
-      let cumulativeCount = 0;
-      const dateWiseClicks = dateWiseClicksResult.map((item) => {
-        cumulativeCount += item.clicks;
-        return {
-          date: item.date,
-          clicks: cumulativeCount,
-        };
-      });
+      // Format date-wise clicks
+      const dateWiseClicks = dateWiseClicksResult.map((item) => ({
+        date: item.date,
+        clicks: item.clicks,
+      }));
   
       // Format device-wise clicks
       const deviceWiseClicks = deviceWiseClicksResult.map((item) => ({
@@ -205,5 +101,6 @@ router.get("/clicks", async (req, res) => {
     }
   });
   
-  module.exports = router;
+
   
+module.exports = router;
